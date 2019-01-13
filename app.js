05cc070289aed5bded7408fad9af05601348af74
Jason <haloos@me.com>
2019-01-13 18:38:10 -0500

    added vailidate email,phone,zip

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function validateName() {
-  const name = document.getElementById('name'); 
+  const name = document.getElementById('name');
   const re = /^[a-zA-Z]{2,10}$/;
 
   if(!re.test(name.value)){
@@ -14,9 +14,9 @@ function validateName() {
     name.classList.remove('is-invalid');
   }
 }
+
 function validateZip() {
-  
-  const zip = document.getElementById('zip'); 
+  const zip = document.getElementById('zip');
   const re = /^[0-9]{5}(-[0-9]{4})?$/;
 
   if(!re.test(zip.value)){
@@ -25,9 +25,25 @@ function validateZip() {
     zip.classList.remove('is-invalid');
   }
 }
+
 function validateEmail() {
-  
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if(!re.test(email.value)){
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
 }
+
 function validatePhone() {
-  
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if(!re.test(phone.value)){
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
 }
